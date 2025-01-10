eb4e0efa999301cf3256de8793f340b16c81ce3a
Sergio Mendolia <sergio.mendolia@liip.ch>
2025-01-10 14:22:50 +0100

    Update .gitignore to exclude test coverage and PHPUnit cache
    Added `tests/coverage/` and `.phpunit.cache/` to the `.gitignore` file to prevent unnecessary files from being tracked. This ensures a cleaner repository and avoids committing local testing artifacts.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,5 @@ use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-
 // https://github.com/symfony/symfony/issues/53812
 set_exception_handler([new ErrorHandler(), 'handleException']);
