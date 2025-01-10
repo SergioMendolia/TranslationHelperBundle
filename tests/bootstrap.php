cf9e706aba279bf147c0c33f49b00320dd39c408
Sergio Mendolia <sergio.mendolia@liip.ch>
2025-01-10 14:18:11 +0100

    Add PHPUnit cache and coverage files for testing
    This commit introduces PHPUnit's code coverage cache and accompanying test coverage assets. These files support improved debugging and analysis of test runs, including code line coverage insights.

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+// https://github.com/symfony/symfony/issues/53812
+set_exception_handler([new ErrorHandler(), 'handleException']);
